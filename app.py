7998833b46d58b12bd4b237979121e5b67cc48fd
Рома Безделов <roma37557@gmail.com>
2024-10-26 18:21:14 +0700

    Имя студента

--- app.py
+++ app.py
@@ -270,4 +270,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Рома Безделов'
+    return render_template('example.html', name=name)
