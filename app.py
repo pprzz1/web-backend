bfc7c43f6fb26069bb48f7d534457286bfde06f8
Рома Безделов <roma37557@gmail.com>
2024-10-26 18:12:55 +0700

    Добавление цветка

--- app.py
+++ app.py
@@ -244,7 +244,7 @@ def a():
 def a2():
     return 'со слэшем'
 
-flower_list = ('роза', 'тюльпан', 'незабудка', 'ромашка')
+flower_list = ['роза', 'тюльпан', 'незабудка', 'ромашка']
 
 @app.route('/lab2/flowers/<int:flower_id>')
 def flowers(flower_id):
@@ -252,3 +252,18 @@ def flowers(flower_id):
         return "такого цветка нет", 404 
     else:
         return 'цветок: ' + flower_list[flower_id]
+    
+@app.route('/lab2/add_flower/<string:name>')
+def add_flower(name):
+    flower_list.append(name)
+    return f'''
+<!doctype html>
+<html>
+    <body>
+    <h1>Добавлен новый цветок</h1>
+    <p>Название нового цветка: {name} </p>
+    <p>Всего цветов: {len(flower_list)} </p>
+    <p>Полный список: {flower_list} </p>
+    </body>
+</html>
+'''
